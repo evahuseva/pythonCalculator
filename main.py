48ae7e9ab4d42a5953d1f7390d7dd2c91f38f16b
evahuseva <55929630+evahuseva@users.noreply.github.com>
2022-10-05 15:24:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,16 @@
-def Numbers(num):
-    return int(num)
-#    return num == '0' or num == '1' or num == '2' or num == '3' or num == '4' or num == '5' or num == '6' or num == '7' or num == '8' or num == '9'
+import re
 
 
-def Test4Num(varstr):
-    null = 0
-    var = ''
-    try:
-        while Numbers(varstr[null]):
-            var += varstr[null]
-            null += 1
-    except: pass
-    return int(var), null
+def calculator(exp):
+    pattern = "[a-zA-Z]+"
+    if re.search(pattern, exp):
+        return "Invalid input! No characters allowed."
+    else:
+        return eval(exp)
 
 
-def operation(string, num1, num2):
-    if string == '+':
-        return num1 + num2
-    if string == '-':
-        return num1-num2
-    if string == '*':
-        return num1*num2
-    if string == '/':
-        return num1/num2
-    if string == '^':
-        return num1 ** num2
+expression = input("Enter the expression: ")
+print(calculator(expression))
 
-
-result = operation(op, number1, number2)
+# r1 = re.findall(r"^\w+",expression)
+# print(r1)
